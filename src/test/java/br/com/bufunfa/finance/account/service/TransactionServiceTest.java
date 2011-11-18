5bc9fbb04cd15c843784d5c53fdc7eb31377c60c
camilo <camiloporto@gmail.com>
2011-11-18 13:56:28 -0300

    adicionado teste de atualizacao de transacao com valor negativo

--- src/test/java/br/com/bufunfa/finance/account/service/TransactionServiceTest.java
+++ src/test/java/br/com/bufunfa/finance/account/service/TransactionServiceTest.java
@@ -369,6 +369,27 @@ public class TransactionServiceTest {
 		
 	}
 	
+	@Test
+	public void testUpdateTransactionToNegativeValue_shouldThrowsException() {
+		AccountSystem accountSystem = serviceTestHelper.createAndSaveAccountSystemSample();
+		Transaction saved = saveSampleTransaction(accountSystem);
+		BigDecimal newValue = new BigDecimal("-100.00");
+		Date date = new GregorianCalendar(2011, Calendar.JANUARY, 1).getTime();
+		String comment = "first year money spent";
+		String expectedTemplateErrorMessage = "{br.com.bufunfa.finance.service.TransactionService.VALUE.negative}";
+		
+		runTestUpdateInvalidTransaction_shouldThrowsException(
+				"should not update transaction to negative value", 
+				saved.getId(),
+				saved.getOriginAccountEntry().getAccount().getId(),
+				saved.getDestAccountEntry().getAccount().getId(),
+				newValue, 
+				date, 
+				comment, 
+				expectedTemplateErrorMessage);
+		
+	}
+	
 	private void runTestUpdateInvalidTransaction_shouldThrowsException(
 			String failMessage,
 			Long idTransaction,
